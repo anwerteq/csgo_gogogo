e088a602b9326f22c15cf6b91453bcd1a242c37e
zml <2497008788@qq.com>
2023-05-26 17:39:49 +0800

    添加优化购买buff商品的逻辑

---
+++ src/main/java/com/chenerzhu/crawler/proxy/pool/csgo/steamentity/SteamSearchRoot.java
@@ -0,0 +1,54 @@
+
+package com.chenerzhu.crawler.proxy.pool.csgo.steamentity;
+import java.util.List;
+public class SteamSearchRoot
+{
+    private boolean success;
+
+    private int start;
+
+    private int pagesize;
+
+    private int total_count;
+
+    private SteamSearchdata steamSearchdata;
+
+    private List<SteamItem> results;
+
+    public void setSuccess(boolean success){
+        this.success = success;
+    }
+    public boolean getSuccess(){
+        return this.success;
+    }
+    public void setStart(int start){
+        this.start = start;
+    }
+    public int getStart(){
+        return this.start;
+    }
+    public void setPagesize(int pagesize){
+        this.pagesize = pagesize;
+    }
+    public int getPagesize(){
+        return this.pagesize;
+    }
+    public void setTotal_count(int total_count){
+        this.total_count = total_count;
+    }
+    public int getTotal_count(){
+        return this.total_count;
+    }
+    public void setSearchdata(SteamSearchdata steamSearchdata){
+        this.steamSearchdata = steamSearchdata;
+    }
+    public SteamSearchdata getSearchdata(){
+        return this.steamSearchdata;
+    }
+    public void setResults(List<SteamItem> results){
+        this.results = results;
+    }
+    public List<SteamItem> getResults(){
+        return this.results;
+    }
+}
