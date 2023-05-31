5b8817c070e16cf8275fb9ed29211e5b588f5868
zhongmeilin <2497008788@qq.com>
2023-06-01 06:12:39 +0800

    xxx

--- src/main/java/com/chenerzhu/crawler/proxy/pool/csgo/service/BuffCostService.java
+++ src/main/java/com/chenerzhu/crawler/proxy/pool/csgo/service/BuffCostService.java
@@ -8,6 +8,7 @@ import com.chenerzhu.crawler.proxy.pool.csgo.repository.BuffCostRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
