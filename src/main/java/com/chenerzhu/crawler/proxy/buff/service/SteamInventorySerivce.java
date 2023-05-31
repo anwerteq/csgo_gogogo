bd0ef0b0eddc6761fd82dcfc3314fa174d0dc198
zml <2497008788@qq.com>
2023-05-31 14:02:00 +0800

    xxxx

--- src/main/java/com/chenerzhu/crawler/proxy/buff/service/SteamInventorySerivce.java
+++ src/main/java/com/chenerzhu/crawler/proxy/buff/service/SteamInventorySerivce.java
@@ -1,6 +1,7 @@
 package com.chenerzhu.crawler.proxy.buff.service;
 
 
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSONObject;
 import com.chenerzhu.crawler.proxy.buff.BuffConfig;
 import com.chenerzhu.crawler.proxy.buff.entity.steamInventory.ManualPlusRoot;
@@ -20,7 +21,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 /**
  * buff上架service
@@ -36,7 +37,7 @@ public class SteamInventorySerivce {
     /**
      * 获取buff中可交易的库存数据
      */
-    public void steamInventory(){
+    public void steamInventory() {
         //查询的为可交易的
         String url = "https://buff.163.com/api/market/steam_inventory?game=csgo&force=0&page_num=1&page_size=50&search=&sort_by=time.desc&state=tradable";
         ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, BuffConfig.getBuffHttpEntity(), String.class);
@@ -48,11 +49,19 @@ public class SteamInventorySerivce {
 
     /**
      * buff自动逻辑
+     *
      * @param items
      */
-    public void manualPlus(List<Items> items){
-        List<Assets> assets = new ArrayList<>();
+    public void manualPlus(List<Items> items) {
+        //需要上架的商品
+        List<Assets> createAssets = new ArrayList<>();
+        //需要取消上架的，订单号
+        List<String> sell_orders = new ArrayList<>();
         for (Items item : items) {
+            //需要先取消上架
+            if (StrUtil.isNotEmpty(item.getSell_order_id())) {
+                sell_orders.add(item.getSell_order_id());
+            }
             Assets asset = new Assets();
             asset.setAssetid(item.getAsset_info().getAssetid());
             asset.setClassid(item.getAsset_info().getClassid());
@@ -62,9 +71,10 @@ public class SteamInventorySerivce {
             Double income = Double.valueOf(asset.getPrice()) * 0.975;
             asset.setIncome(income.toString());
             asset.setInstanceid(item.getAsset_info().getInstanceid());
-            assets.add(asset);
+            createAssets.add(asset);
         }
-        sellOrderCreate(assets);
+        cancelOrder(sell_orders);
+        sellOrderCreate(createAssets);
 //        String url = "https://buff.163.com/market/sell_order/preview/manual_plus";//post
 //        HttpHeaders headers = BuffConfig.getHeaderMap();
 //        headers.add("X-Csrftoken",  BuffConfig.getCookieOnlyKey("csrf_token"));
@@ -76,26 +86,62 @@ public class SteamInventorySerivce {
     }
 
 
-    public void sellOrderCreate(List<Assets> assets){
+    /**
+     * 设置buff上架
+     *
+     * @param assets
+     */
+
+    public void sellOrderCreate(List<Assets> assets) {
         HttpHeaders headers = BuffConfig.getHeaderMap();
         headers = new HttpHeaders();
         headers.add("X-Csrftoken", BuffConfig.getCookieOnlyKey("csrf_token"));
         headers.add("Referer", "https://buff.163.com/market/steam_inventory?game=csgo");
         headers.add("Origin", "https://buff.163.com");
-        headers.add("Cookie",BuffConfig.getCookie());
+        headers.add("Cookie", BuffConfig.getCookie());
         ManualPlusRoot manualPlusRoot = new ManualPlusRoot();
         manualPlusRoot.setAssets(assets);
         System.out.println(JSONObject.toJSONString(manualPlusRoot));
         HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity(JSONObject.parseObject(JSONObject.toJSONString(manualPlusRoot), HashMap.class), headers);
         String url = "https://buff.163.com/api/market/sell_order/create/manual_plus";
         ResponseEntity<String> responseEntity1 = restTemplate.exchange(url, HttpMethod.POST, httpEntity, String.class);
-        if (responseEntity1.getStatusCode().value() != 200){
+        if (responseEntity1.getStatusCode().value() != 200) {
             //获取失败
         }
         JSONObject jsonObject = JSONObject.parseObject(responseEntity1.getBody());
-        if (!jsonObject.getString("code").equals("OK")){
+        if (!jsonObject.getString("code").equals("OK")) {
             //接口返回不成功
         }
         log.info("buff上架成功");
+    }
+
+
+    /**
+     * buff上架的商品取消上架
+     */
+    public void cancelOrder(List<String> sell_orders) {
+        String url = "https://buff.163.com/api/market/sell_order/cancel";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Csrftoken", BuffConfig.getCookieOnlyKey("csrf_token"));
+        headers.add("Referer", "https://buff.163.com/market/sell_order/on_sale?game=csgo&mode=2,5");
+        headers.add("Origin", "https://buff.163.com");
+        headers.add("Cookie", BuffConfig.getCookie());
+        //参数集合
+        Map<String, Object> para = new HashMap();
+        para.put("game", "csgo");
+        para.put("sell_orders", sell_orders);
+
+        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity(para, headers);
+        ResponseEntity<String> responseEntity1 = restTemplate.exchange(url, HttpMethod.POST, httpEntity, String.class);
+        if (responseEntity1.getStatusCode().value() != 200) {
+            //获取失败
+        }
+        JSONObject jsonObject = JSONObject.parseObject(responseEntity1.getBody());
+        if (!jsonObject.getString("code").equals("OK")) {
+            //接口返回不成功
+        }
+
+        log.info("buff取消上架成功");
+
     }
 }
