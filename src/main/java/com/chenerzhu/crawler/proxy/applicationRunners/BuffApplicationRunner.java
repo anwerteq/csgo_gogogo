26f6d2c0429f03810101154d6802d8e14bcab67c
zml <2497008788@qq.com>
2023-10-11 15:55:13 +0800

    增加配置

--- src/main/java/com/chenerzhu/crawler/proxy/applicationRunners/BuffApplicationRunner.java
+++ src/main/java/com/chenerzhu/crawler/proxy/applicationRunners/BuffApplicationRunner.java
@@ -3,6 +3,7 @@ package com.chenerzhu.crawler.proxy.applicationRunners;
 import cn.hutool.core.util.StrUtil;
 import com.chenerzhu.crawler.proxy.buff.BuffUserData;
 import com.chenerzhu.crawler.proxy.cache.BuffCacheService;
+import com.chenerzhu.crawler.proxy.steam.util.SleepUtil;
 import com.chenerzhu.crawler.proxy.util.bufflogin.BuffAccountInfoConfig;
 import com.chenerzhu.crawler.proxy.util.bufflogin.BuffAutoLoginUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -54,6 +55,14 @@ public class BuffApplicationRunner implements ApplicationRunner {
                 break;
             }
             buffUserDataList.add(buffUserData);
+        }
+        for (BuffUserData buffUserData : buffUserDataList) {
+            log.info("加载buff账号：{}成功",buffUserData.getAcount());
+        }
+        if(buffUserDataList.isEmpty()){
+            log.error("未加载到buff账号，请检查[buff.account_information]配置,退出脚本中");
+            SleepUtil.sleep(5000);
+            System.exit(1);
         }
     }
 }
