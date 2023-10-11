d0673fbc6de7a5441a7143cafc6d574ed581d609
zml <2497008788@qq.com>
2023-10-11 15:51:41 +0800

    增加配置

--- src/main/java/com/chenerzhu/crawler/proxy/applicationRunners/BuffAutoSaleRunner.java
+++ src/main/java/com/chenerzhu/crawler/proxy/applicationRunners/BuffAutoSaleRunner.java
@@ -5,6 +5,7 @@ import com.chenerzhu.crawler.proxy.buff.service.SteamInventorySerivce;
 import com.chenerzhu.crawler.proxy.config.CookiesConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.annotation.Order;
@@ -23,13 +24,23 @@ public class BuffAutoSaleRunner implements ApplicationRunner {
     @Autowired
     SteamInventorySerivce steamInventorySerivce;
 
+    @Value("${buff_on_the_shelves}")
+    private Boolean buff_on_the_shelves;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        List<BuffUserData> buffUserDataList = BuffApplicationRunner.buffUserDataList;
-        for (BuffUserData buffUserData : buffUserDataList) {
-            BuffApplicationRunner.buffUserDataThreadLocal.set(buffUserData);
-            CookiesConfig.buffCookies.set(buffUserData.getCookie());
-            steamInventorySerivce.autoSale();
+        if (!buff_on_the_shelves){
+            return;
+        }
+        try{
+            List<BuffUserData> buffUserDataList = BuffApplicationRunner.buffUserDataList;
+            for (BuffUserData buffUserData : buffUserDataList) {
+                BuffApplicationRunner.buffUserDataThreadLocal.set(buffUserData);
+                CookiesConfig.buffCookies.set(buffUserData.getCookie());
+                steamInventorySerivce.autoSale();
+            }
+        }catch (Exception e){
+
         }
 
     }
