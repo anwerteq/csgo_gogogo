f6e2c112812c72076eea585728ddff4a844d6e61
xiaojuzi <2497008788@qq.com>
2024-12-05 01:01:30 +0800

    初始化st项目

---
+++ st-management/src/main/java/com/xiaojuzi/stmanagement/StManagementApplication.java
@@ -0,0 +1,13 @@
+package com.xiaojuzi.stmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StManagementApplication.class, args);
+	}
+
+}
---
+++ st-management/src/test/java/com/xiaojuzi/stmanagement/StManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaojuzi.stmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
